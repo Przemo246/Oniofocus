d0b5399ebacfa69cb3ee9402252a1ec468ff273f
przemo246 <przemo246@gmail.com>
2023-01-03 01:23:52 +0100

    setup vue app

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <label htmlFor="my-modal-4" className="btn">open modal</label>
+
+  <input type="checkbox" id="my-modal-4" className="modal-toggle" />
+  <label htmlFor="my-modal-4" className="modal cursor-pointer">
+    <label className="modal-box relative" htmlFor="">
+      <label
+        for="my-modal-4"
+        class="btn btn-sm btn-circle absolute right-2 top-2"
+        >✕</label
+      >
+      <h3 className="text-lg font-bold">
+        Congratulations random Internet user!
+      </h3>
+      <p className="py-4">
+        You've been selected for a chance to get one year of subscription to use
+        Wikipedia for free!
+      </p>
+    </label>
+  </label>
+</template>
